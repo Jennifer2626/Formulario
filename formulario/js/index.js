e562ea9c8f245aa3fcb640e3001bfd39f2613a5b
Jennifer2626 <125832197+Jennifer2626@users.noreply.github.com>
2023-02-19 22:12:33 +0100

    Add files via upload

---
+++ formulario/js/index.js
@@ -0,0 +1,56 @@
+const formulario = document.getElementById('formulario');
+
+const expresiones = {
+	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/, 
+	correo: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+	password: /^.{4,8}$/,
+}
+
+const campos = {
+	nombre: false
+	correo: false,
+	password: false,
+}
+
+const validarCampo = (expresion, input, campo) => {
+	if(expresion.test(input.value)){
+		document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-incorrecto');
+		document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-correcto');
+		document.querySelector(`#grupo__${campo} i`).classList.add('...img src="imagenes/v.png" height="25px" width="25px"');
+		document.querySelector(`#grupo__${campo} i`).classList.remove(''...img src="imagenes/x.png" height="25px" width="25px"'');
+		document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.remove('formulario__input-error-activo');
+		campos[campo] = true;
+	} else {
+		document.getElementById(`grupo__${campo}`).classList.add('formulario__grupo-incorrecto');
+		document.getElementById(`grupo__${campo}`).classList.remove('formulario__grupo-correcto');
+		document.querySelector(`#grupo__${campo} i`).classList.add('...img src="imagenes/v.png" height="25px" width="25px"');
+		document.querySelector(`#grupo__${campo} i`).classList.remove('...img src="imagenes/x.png" height="25px" width="25px"');
+		document.querySelector(`#grupo__${campo} .formulario__input-error`).classList.add('formulario__input-error-activo');
+		campos[campo] = false;
+	}
+}
+
+const validarFormulario = (e) => {
+	switch (e.target.name) {
+		case "nombre":
+			validarCampo(expresiones.nombre, e.target, 'nombre');
+		break;
+		case "correo":
+			validarCampo(expresiones.correo, e.target, 'correo');
+		break;
+		case "password":
+			validarCampo(expresiones.password, e.target, 'password');
+			validarPassword2();
+		break;
+		case "password2":
+			validarPassword2();
+		break;
+	}
+}
+
+const validarPassword2 = () => {
+	const inputPassword1 = document.getElementById('password');
+	const inputPassword2 = document.getElementById('password2');
+	};
+	
+	
